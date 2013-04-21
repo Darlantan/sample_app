4827dedb6e6b60c80219b49f8bfc9fccdbef0c65
Iiro Vaahtojarvi <iirovaahtojarvi@gmail.com>
2013-04-21 16:25:40 +0300

    Add "contact" static page and related tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,9 +10,9 @@ describe "Static pages" do
     end
 
     it "should have the title 'Home'" do
-	  visit '/static_pages/home'
-	  page.should have_selector('title', :text => " | Home")
-	end
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => " | Home")
+    end
   end
 
   describe "Help page" do
@@ -23,9 +23,9 @@ describe "Static pages" do
     end
 
     it "should have the title 'Help'" do
-	  visit '/static_pages/help'
-	  page.should have_selector('title', :text => " | Help")
-	end
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => " | Help")
+    end
   end
 
   describe "About page" do
@@ -36,8 +36,22 @@ describe "Static pages" do
     end
 
     it "should have the title 'About Us'" do
-	  visit '/static_pages/about'
-	  page.should have_selector('title', :text => " | About Us")
-	end
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => " | About Us")
+    end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "Should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => ' | Contact')
+    end
+  end
+
 end
